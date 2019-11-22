5875488313ec9896eed959b5c7ee18e86c3a4427
unknown <calvinp003@gmail.com>
2019-11-22 09:37:01 -0800

    link user input to search terms in api call

--- src/main.js
+++ src/main.js
@@ -2,3 +2,18 @@ import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { DoctorService } from './../src/backend.js'
+
+
+$(document).ready(function(){
+  let docService = new DoctorService;
+  $(".docSearch").submit(function(e){
+    e.preventDefault();
+    let name = $("#docName").val();
+    let symptom = $("#symptom").val();
+    console.log(name, symptom);
+    (async function docSearch() {
+      let response = await docService.findDoctors(name, symptom);
+      console.log(response);
+  })();
+});
+});
