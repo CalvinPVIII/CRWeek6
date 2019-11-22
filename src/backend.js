70abfe98e519525dd8b7e4bbf1e2f3024dd01101
unknown <calvinp003@gmail.com>
2019-11-22 15:44:17 -0800

    begin implementing geo api

--- src/backend.js
+++ src/backend.js
@@ -2,11 +2,20 @@ export class DoctorService {
 
   async findDoctors(name,search){
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${search}&location=45.523062%2C-122.676482%2C25&user_location=45.523062%2C-122.676482&skip=0&limit=5&user_key=${process.env.API_KEY}`)
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${search}&location=${lat}%2C-${lon}&user_location=${lat}%2C-${lon}&skip=0&limit=5&user_key=${process.env.API_KEY}`)
         let jsonifiedResponse = await response.json();
         return jsonifiedResponse;
     }catch (error){
       return "There was an error handling your request: " + error.message;
     }
   }
+  async getLocationInfo(location){
+    try{
+      let response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${process.env.GEO_KEY} `);
+        let jsonifiedResponse = await response.json();
+        return jsonifiedResponse;
+      }catch (error){
+        return "There was an error handling your request: " + error.message;
+      }
+    }
 }
