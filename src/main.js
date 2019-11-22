0efee40f200f3b76fe9a062c78088f089e49b5c7
unknown <calvinp003@gmail.com>
2019-11-22 10:27:16 -0800

    add display toggle when no results are found

--- src/main.js
+++ src/main.js
@@ -13,18 +13,25 @@ $(document).ready(function(){
     console.log(name, symptom);
     (async function docSearch() {
       let response = await docService.findDoctors(name, symptom);
-      console.log(response);
-      $("#docImage").attr('src', response.data[0].profile.image_url)
-      $("#first").text(response.data[0].profile.first_name)
-      $("#middle").text(response.data[0].profile.middle_name)
-      $("#last").text(response.data[0].profile.last_name)
-      $("#workplace").text(response.data[0].practices[0].name)
-      $('#address').text(`${response.data[0].practices[0].visit_address.street}, ${response.data[0].practices[0].visit_address.city}, ${response.data[0].practices[0].visit_address.state_long}, ${response.data[0].practices[0].visit_address.zip}`)
-      $("#number").text(response.data[0].practices[0].phones[0].number)
-      $("#website").text(response.data[0].practices[0].website)
-      $("#website").attr('href', response.data[0].practices[0].website)
-      if (response.data[0].practices[0].accepts_new_patients === true) {
-        $("#acceptingPatients").text(`${response.data[0].profile.first_name} ${response.data[0].profile.last_name} is accepting new patients.`)
+      console.log(response.data[0]);
+      if (response.data[0] === undefined) {
+        $(".noResult").show();
+        $(".results").hide();
+      }else {
+        $(".noResult").hide();
+        $(".results").show();
+        $("#docImage").attr('src', response.data[0].profile.image_url)
+        $("#first").text(response.data[0].profile.first_name)
+        $("#middle").text(response.data[0].profile.middle_name)
+        $("#last").text(response.data[0].profile.last_name)
+        $("#workplace").text(response.data[0].practices[0].name)
+        $('#address').text(`${response.data[0].practices[0].visit_address.street}, ${response.data[0].practices[0].visit_address.city}, ${response.data[0].practices[0].visit_address.state_long}, ${response.data[0].practices[0].visit_address.zip}`)
+        $("#number").text(response.data[0].practices[0].phones[0].number)
+        $("#website").text(response.data[0].practices[0].website)
+        $("#website").attr('href', response.data[0].practices[0].website)
+        if (response.data[0].practices[0].accepts_new_patients === true) {
+          $("#acceptingPatients").text(`${response.data[0].profile.first_name} ${response.data[0].profile.last_name} is accepting new patients.`)
+        }
       }
   })();
 });
