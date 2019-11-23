54c330763f26b4175fd93cfda493df95dae8a7f6
unknown <calvinp003@gmail.com>
2019-11-22 16:41:45 -0800

    add location search option

--- src/backend.js
+++ src/backend.js
@@ -1,21 +1,21 @@
 export class DoctorService {
 
-  async findDoctors(name,search){
+  async findDoctors(name, search, lat, lon) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${search}&location=${lat}%2C-${lon}&user_location=${lat}%2C-${lon}&skip=0&limit=5&user_key=${process.env.API_KEY}`)
-        let jsonifiedResponse = await response.json();
-        return jsonifiedResponse;
-    }catch (error){
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${search}&location=${lat}%2C${lon}%2C25&user_location=${lat}%2C${lon}&skip=0&limit=5&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch (error) {
       return "There was an error handling your request: " + error.message;
     }
   }
-  async getLocationInfo(location){
-    try{
-      let response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${process.env.GEO_KEY} `);
-        let jsonifiedResponse = await response.json();
-        return jsonifiedResponse;
-      }catch (error){
-        return "There was an error handling your request: " + error.message;
-      }
+  async getLocationInfo(location) {
+    try {
+      let response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=a2b385b155014d69827dc9d9e7393c86`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch (error) {
+      return "There was an error handling your request: " + error.message;
     }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,65 +1,71 @@
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { DoctorService } from './../src/backend.js'
+import {
+  DoctorService
+} from './../src/backend.js';
 
 const addDocToUi = (number, response) => {
 
-if (typeof response == 'string' && response.includes("There was an error handling your request")) {
-  $("#error").text(response);
-  $(".noResult").show();
-  $(".results").hide();
-
-}else {
-  if (response.data[0] === undefined) {
+  if (typeof response == 'string' && response.includes("There was an error handling your request")) {
+    $("#error").text(response);
     $(".noResult").show();
     $(".results").hide();
-  }else {
-    if (response.data[number] === undefined) {
-      $(`#doc${number}`).hide();
 
-    }else {
-      $(".results").fadeIn();
-      $(".noResult").hide();
+  } else {
+    if (response.data[0] === undefined) {
+      $(".noResult").show();
+      $(".results").hide();
+    } else {
+      if (response.data[number] === undefined) {
+        $(`#doc${number}`).hide();
 
-      $(`#doc${number}`).show();
-      $(`#docImage${number}`).attr('src', response.data[number].profile.image_url)
-      $(`#docName${number}`).text(`${response.data[number].profile.first_name}  ${response.data[number].profile.last_name}`)
-      $(`#workplace${number}`).text(response.data[number].practices[0].name)
-      $(`#address${number}`).text(`${response.data[number].practices[0].visit_address.street}, ${response.data[number].practices[0].visit_address.city}, ${response.data[number].practices[0].visit_address.state_long}, ${response.data[number].practices[0].visit_address.zip}`)
-      $(`#number${number}`).text(response.data[number].practices[0].phones[0].number)
-      if (response.data[number].practices[0].website === undefined) {
-        $(`#website${number}`).text('')
-        $(`#website${number}`).attr('')
-      }else {
-        $(`#website${number}`).text(response.data[number].practices[0].website)
-        $(`#website${number}`).attr('href', response.data[number].practices[0].website)
-      }
-      if (response.data[number].practices[0].accepts_new_patients === true) {
-        $(`#acceptingPatients${number}`).text(`${response.data[number].profile.first_name} ${response.data[number].profile.last_name} is accepting new patients.`)
-      }else {
-        $(`#acceptingPatients${number}`).text(`${response.data[number].profile.first_name} ${response.data[number].profile.last_name} is not accepting new patients.`)
+      } else {
+        $(".results").fadeIn();
+        $(".noResult").hide();
+
+        $(`#doc${number}`).show();
+        $(`#docImage${number}`).attr('src', response.data[number].profile.image_url);
+        $(`#docName${number}`).text(`${response.data[number].profile.first_name}  ${response.data[number].profile.last_name}`);
+        $(`#workplace${number}`).text(response.data[number].practices[0].name);
+        $(`#address${number}`).text(`${response.data[number].practices[0].visit_address.street}, ${response.data[number].practices[0].visit_address.city}, ${response.data[number].practices[0].visit_address.state_long}, ${response.data[number].practices[0].visit_address.zip}`);
+        $(`#number${number}`).text(response.data[number].practices[0].phones[0].number);
+        if (response.data[number].practices[0].website === undefined) {
+          $(`#website${number}`).text('');
+          $(`#website${number}`).attr('');
+        } else {
+          $(`#website${number}`).text(response.data[number].practices[0].website);
+          $(`#website${number}`).attr('href', response.data[number].practices[0].website);
+        }
+        if (response.data[number].practices[0].accepts_new_patients === true) {
+          $(`#acceptingPatients${number}`).text(`${response.data[number].profile.first_name} ${response.data[number].profile.last_name} is accepting new patients.`);
+        } else {
+          $(`#acceptingPatients${number}`).text(`${response.data[number].profile.first_name} ${response.data[number].profile.last_name} is not accepting new patients.`);
+        }
       }
     }
-    }
-}
-}
+  }
+};
 
 
-$(document).ready(function(){
+$(document).ready(function() {
   let docService = new DoctorService;
-  $(".docSearch").submit(function(e){
+  $(".docSearch").submit(function(e) {
     $(".results").fadeOut();
     e.preventDefault();
     let name = $("#docName").val();
     let symptom = $("#symptom").val();
-    (async function docSearch() {
-      let response = await docService.findDoctors(name, symptom);
-      addDocToUi(0, response)
-      addDocToUi(1, response)
-      addDocToUi(2, response)
-      addDocToUi(3, response)
-      addDocToUi(4, response)
-  })();
-});
+    let userLocation = $("#location").val();
+    (async function display() {
+      let location = await docService.getLocationInfo(userLocation);
+      let lat = location.results[0].geometry.lat;
+      let lon = location.results[0].geometry.lng;
+      let response = await docService.findDoctors(name, symptom, lat, lon);
+      addDocToUi(0, response);
+      addDocToUi(1, response);
+      addDocToUi(2, response);
+      addDocToUi(3, response);
+      addDocToUi(4, response);
+    })();
+  });
 });
